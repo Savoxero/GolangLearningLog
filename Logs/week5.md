153ed86915664605405a3968c3db35097abe0c8f
WR <marbxlmar@gmail.com>
2025-10-12 23:44:35 +0200

    continuous calculation completed. end of week5

--- Logs/week5.md
+++ Logs/week5.md
@@ -7,11 +7,32 @@
 - im gonna refine the calculator and finish it. 
 - [] adding a way to track every past calculations(history with a slice) 
 - [] moving the calculations into its own package( not a feature )
-- [] continuous calculation
+- [x]continuous calculation
 - [] word counter using map[string] int
 - [] a system to automaticly exit if the input is repeated and the program does not execute as designed.
 ## Progress: 
 progress and toughts:
 currently im implementing the continous calculations ive been thinking of if operators in order for the calculations to never fuck up and be consistent. the code is pretty self explanatory but i believe i implemented alot of unecessary complex design patterns. if i could simplify it i would, but i lack knowledge.
+## End of week 5 progress. 
+- currently im fixing the issue within the continous calculation, i have to redefine the result one more time, it was hard to track this thing down. i forgot to essentially update the second loop iteration with the fresh result. i tought it was some variable issue but it was not
+- i learned a bunch of stuff about git and github, push pull merge, commit. I have learned how to organize it better aswell.
+- I am gonna resume boot.dev once i am done with my calculator.
 ## Random blabber
 - been fun thinking about designs and how to potentially implement specific features at work. programming is truly an art of it's own. sinking my time inside of it has been as good as it gets whether games, work, reading mangas, entertainment. that kind of stuff feels good but it ain't productive and it doesnt really challenge you in such a capacity as coding and problem solving does.
+## Summary
+- I finished my calculator along with error handling, continous calculations, took me quite a while, i'm thinking of adding more features to it. I believe it's a pretty clean and understandable code. i will document it so it makes sense
+- i cleaned up my calculator code with the help of claude *he gave me some useful insights as to how to clean it up and make it more readable*, added comments, completely scrapped my past switch logic with a new variable ( much, much cleaner than before) here is an example:
+![alt text](images/switchstat.png) night and day difference ( i didn't think it was possible to do that, but now that i know of it it makes so much sense.)
+
+
+- _things i wanna achieve in the context of the calculator_
+Add history with a slice (print past calculations).
+Move math ops into their own package.
+Word Counter: Input a sentence → return frequency of each word using map[string]int.
+len and cap understanding and implementation
+- here are some things i want to achieve
+Can you explain slices vs arrays and demonstrate len/cap?
+Can you handle user input without panic on bad data? *( i believe this one in the context of the calculator is done).*
+Can you split code into packages and import correctly?
+
+ALOT OF WORK ahead of me 
--- Projects/Calculator/deroda.go
+++ Projects/Calculator/deroda.go
@@ -6,45 +6,36 @@ import (
 )
 
 func main() {
-	var num1 float64
-	var num2 float64
-	var operation string
-	var input string
-	var filterednumb float64
+	var num1 float64         /// first number
+	var num2 float64         /// second number
+	var operation string     /// operations + - * /
+	var input string         /// a variable created for error handling and filter the user's inputs
+	var filterednumb float64 /// variable that sucesfully went trough the filtering process
 	var err error
-	var finalnumresult float64
-	var resultA float64
-	var calculationcheck = true
-
+	var finalnumresult float64 ///final result of each calculation, used for every calculations past the first one
+	var calculationcheck bool  /// boolean variable that decides in which order should the calculation play out
+	var firstOperand float64   /// to make the switch statement cleaner
 	for {
-
-		for {
-			if num1 > 0 {
-				break
-			}
-			fmt.Print("Enter first number: ")
+		if calculationcheck == false { /// first for loop, it checks for calculation check, its set to false in default so it executes it first
+			fmt.Print("Enter first number: ") /// user input if its exit, exits the entire program,  it scans for a string that isnt exit, continues the loop till a number is the input.
 			fmt.Scan(&input)
 			if input == "exit" {
 				fmt.Println("thank you, come again")
 				return
 			}
-
 			filterednumb, err = strconv.ParseFloat(input, 64)
-			calculationcheck = false
 			if err != nil {
 				fmt.Println("Please Enter a valid number")
 				continue
 			}
-			break
+			num1 = filterednumb /// after the filtering process num 1 becomes the user's input for future calculations, when the first iteration has been complete. it becomes obsolete
+
 		}
-
-		num1 = filterednumb
-
 		for {
-			fmt.Print("Enter operation (+, -, *, /): ")
+			fmt.Print("Enter operation (+, -, *, /): ") /// operations, if the user's operation is not equal to any of the listed operations, it'll repeat the message.
 			fmt.Scan(&operation)
 			if operation == "exit" {
-				fmt.Println("thank you, come again")
+				fmt.Println("thank you, come again") /// the condition of breaking the loop is entering a valid operation, else the loop continues looping.
 				return
 			}
 			if operation == "+" || operation == "/" || operation == "-" || operation == "*" || operation == "exit" {
@@ -57,72 +48,48 @@ func main() {
 		}
 
 		for {
-			fmt.Print("Enter second number: ")
+			fmt.Print("Enter second number: ") ///same as num 1 filters, exit is exiting the program
 			fmt.Scan(&input)
 			if input == "exit" {
 				return
 			}
-			filterednumb, err = strconv.ParseFloat(input, 64)
+			filterednumb, err = strconv.ParseFloat(input, 64) /// and loops when its a string and not a number.
 			if err != nil {
 				fmt.Println("Please Enter a valid number")
 				continue
 			}
 			break
 		}
+		num2 = filterednumb ///filtered number 2 for further calculations. unlike num1, its necessary to put it outside the loop for further calculations.
 
-		num2 = filterednumb
-
-		if calculationcheck == true {
-			switch operation {
-			case "-":
-				fmt.Println(subtract(finalnumresult, num2))
-
-			case "*":
-				fmt.Println(multiply(finalnumresult, num2))
-
-			case "/":
-				result, err := divide(finalnumresult, num2)
-				if err != nil {
-					fmt.Println(err)
-					break
-				}
-				fmt.Println(result)
-
-			case "+":
-				fmt.Println(add(finalnumresult, num2))
-
-			default:
-				fmt.Println("Unknown operation")
+		if calculationcheck == false { /// the boolean logic here is the foundation of everything, its a on and off switch, except in this scenario the switch stays on. u click it on and it stays on, after num 1 and num 2 produce the final result, num 1 becomes entirely obsolete
+			firstOperand = num1
+			calculationcheck = true ///calculationcheck = true  // Switch to continuous mode - all future calculations use previous result
+		} else {
+			firstOperand = finalnumresult
+		}
+		switch operation {
+		case "-":
+			fmt.Println(subtract(firstOperand, num2))
+			finalnumresult = subtract(firstOperand, num2)
+		case "*":
+			fmt.Println(multiply(firstOperand, num2))
+			finalnumresult = multiply(firstOperand, num2)
+		case "/":
+			result, err := divide(firstOperand, num2)
+			if err != nil {
+				fmt.Println(err)
+				break
 			}
-		} else if calculationcheck == false {
+			fmt.Println(result)
+			finalnumresult = result
+		case "+":
+			fmt.Println(add(firstOperand, num2))
+			finalnumresult = add(firstOperand, num2)
 
-			switch operation {
-			case "-":
-				fmt.Println(subtract(num1, num2))
-				resultA = subtract(num1, num2)
-			case "*":
-				fmt.Println(multiply(num1, num2))
-				resultA = multiply(num1, num2)
-			case "/":
-				result, err := divide(num1, num2)
-				if err != nil {
-					fmt.Println(err)
-					break
-				}
-				fmt.Println(result)
-				resultA = result
-			case "+":
-				fmt.Println(add(num1, num2))
-				resultA = add(num1, num2)
-
-			default:
-				fmt.Println("Unknown operation")
-			}
-
-			finalnumresult = resultA
-
-			fmt.Printf("The starting number is now %f", finalnumresult)
+		default:
+			fmt.Println("Unknown operation")
 		}
 
-	}
-}
+	} /// this sums it up, finalnumresult takes on multiple values troughout the calculation process, and my calculator.go has all the calculation code.
+} /// pretty simple code, but it works
